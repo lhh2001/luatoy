3799b8f7b6a88ebc63c2c5da1fc3d70d8fb467bd
henghaoli <henghaoli@tencent.com>
2025-01-18 14:34:34 +0800

    实现?.语法

--- lualib/lopcodes.c
+++ lualib/lopcodes.c
@@ -36,6 +36,7 @@ LUAI_DDEF const lu_byte luaP_opmodes[NUM_OPCODES] = {
  ,opmode(0, 0, 0, 0, 1, iABC)		/* OP_GETTABLE */
  ,opmode(0, 0, 0, 0, 1, iABC)		/* OP_GETI */
  ,opmode(0, 0, 0, 0, 1, iABC)		/* OP_GETFIELD */
+ ,opmode(0, 0, 0, 0, 1, iABC)		/* OP_SAFEGETFIELD */
  ,opmode(0, 0, 0, 0, 0, iABC)		/* OP_SETTABUP */
  ,opmode(0, 0, 0, 0, 0, iABC)		/* OP_SETTABLE */
  ,opmode(0, 0, 0, 0, 0, iABC)		/* OP_SETI */
